e22f06f641dc532acd7c3e7136632418b193d533
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2025-04-12 15:11:47 -0300

    chore(docker): configurando compose

---
+++ backend/src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as crypto from 'crypto';
+
+Object.defineProperty(global, 'crypto', {
+  value: {
+    ...crypto,
+    subtle: crypto.webcrypto.subtle,
+  },
+});
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: ['dist/**/*.entity.js'],
+        synchronize: false,
+      }),
+    }),
+  ],
+
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
